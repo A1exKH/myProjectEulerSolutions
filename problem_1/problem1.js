924e6252ad5d06c087ed657e3f496e78f00ab4ce
A1exKH <alexmkul@gmail.com>
2019-05-05 00:04:14 +0300

    Add code for problem 1.

---
+++ problem_1/problem1.js
@@ -0,0 +1,23 @@
+// Problem 1. Multiples of 3 and 5
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+function findSumOfMultiples()
+{
+	// time: 0 seconds.
+	
+	var d0 = new Date();
+	var sum =0;
+	
+	for(var i=0; i<1000; i++)
+	{
+		if(i%3==0 || i%5==0)
+		{
+			sum += i;
+		}
+	}
+	console.log("Answer: " + sum);
+	var d1 = new Date();
+	console.log("time: " + (d1.getTime() - d0.getTime())/1000 + " seconds.");
+}
+
+findSumOfMultiples();
