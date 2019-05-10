4f20a3feec3f636c9a72b1b290aa804c1bd41910
Alex Kulinkovich <alexmkul@gmail.com>
2019-05-10 10:39:58 +0300

    Add code for problem 10.

---
+++ problem_10/problem10.js
@@ -0,0 +1,41 @@
+// Problem 10. Summation of primes
+// Find the sum of all the primes below two million.
+
+function findSummationOfPrimes(num)
+{
+	
+	// time: 815.252 seconds.
+	
+	var d0 = new Date();
+	var sum = 0;
+	for(var i=2; i<num; i++)
+	{
+		if(isPrime(i))
+		{
+			sum += i;
+		}
+	}
+	console.log("Answer: " + sum);
+	var d1 = new Date();
+	console.log("time: " + (d1.getTime() - d0.getTime())/1000 + " seconds.");
+}
+
+function isPrime(num)
+{
+	var factorsCounter = 0;
+	for(var i = 1; i < num; i++)
+	{
+		if(num % i == 0)
+		{
+			factorsCounter++;
+			if(factorsCounter > 1)
+			{
+				return false;
+				break;
+			}
+		}
+	}
+	return true;
+}
+
+findSummationOfPrimes(2000000);
