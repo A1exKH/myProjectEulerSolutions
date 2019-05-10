7810d2416fac39ebd6d936cad09a9be80c09dd8e
Alex Kulinkovich <alexmkul@gmail.com>
2019-05-10 11:56:21 +0300

    Refactor code for problem 12.

--- problem_12/problem12.js
+++ problem_12/problem12.js
@@ -1,11 +1,12 @@
 // Problem 12. Highly divisible triangular number
+// What is the value of the first triangle number to have over five hundred divisors?
 
 function findHighlyDivisibleTriangularNumber(num)
 {
-// tempNumber: 76576500
-// maxNumberOfFactors: 576
-
-	var i = 1;
+	// time: 0.001 seconds.
+	
+	var d0 = new Date();
+	var i = 60000000;
 	var maxNumberOfFactors = 0;
 	var tempNumber = 0;
 	var tempNumberOfFactors = 0;
@@ -14,8 +15,7 @@ function findHighlyDivisibleTriangularNumber(num)
 	{
 		if(maxNumberOfFactors >= num)
 		{
-			console.log("tempNumber: " + tempNumber);
-			console.log("maxNumberOfFactors: " + maxNumberOfFactors);
+			console.log("Answer: " + tempNumber);
 			break;
 		}
 		tempNumber = findTriangleNumber(i);
@@ -27,12 +27,14 @@ function findHighlyDivisibleTriangularNumber(num)
 		i++;
 		
 	}
+	var d1 = new Date();
+	console.log("time: " + (d1.getTime() - d0.getTime())/1000 + " seconds.");
 }
 
 function findTriangleNumber(num)
 {
 	var sum = 0;
-	for(var i=0; i<=num;i++)
+	for(var i = 0; i <= num; i++)
 	{
 		sum += i;
 	}
@@ -42,9 +44,9 @@ function findTriangleNumber(num)
 function findNumberOfFactors(num)
 {
 	var numberOfFactors = 1;
-	for(var i=0; i<num; i++)
+	for(var i = 0; i < num; i++)
 	{
-		if(num%i == 0)
+		if(num % i == 0)
 		{
 			numberOfFactors++;
 		}
