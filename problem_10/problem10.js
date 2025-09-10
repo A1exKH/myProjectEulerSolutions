1e6ae315fc76b42ccb72e312bb227fedf52984c7
A1exKH <alexmkul@gmail.com>
2025-09-10 23:13:21 +0200

    Fixed code formatting for problem10.

--- problem_10/problem10.js
+++ problem_10/problem10.js
@@ -2,33 +2,32 @@
 // Find the sum of all the primes below two million.
 
 function findSummationOfPrimes(num)
-{
-	
+{	
 	// time: 815.252 seconds.
 	
 	var d0 = new Date();
 	var sum = 0;
-	for(var i=2; i<num; i++)
+	for (var i = 2; i < num; i++)
 	{
-		if(isPrime(i))
+		if (isPrime(i))
 		{
 			sum += i;
 		}
 	}
 	console.log("Answer: " + sum);
 	var d1 = new Date();
-	console.log("time: " + (d1.getTime() - d0.getTime())/1000 + " seconds.");
+	console.log("time: " + (d1.getTime() - d0.getTime()) / 1000 + " seconds.");
 }
 
 function isPrime(num)
 {
 	var factorsCounter = 0;
-	for(var i = 1; i < num; i++)
+	for (var i = 1; i < num; i++)
 	{
-		if(num % i == 0)
+		if (num % i == 0)
 		{
 			factorsCounter++;
-			if(factorsCounter > 1)
+			if (factorsCounter > 1)
 			{
 				return false;
 				break;
