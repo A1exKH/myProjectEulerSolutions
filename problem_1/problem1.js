6f9f4059669a79e1016e22aca6b5f22eb1eb116e
A1exKH <alexmkul@gmail.com>
2025-09-10 22:48:10 +0200

    Fixed code formatting for problem1.

--- problem_1/problem1.js
+++ problem_1/problem1.js
@@ -8,9 +8,9 @@ function findSumOfMultiples()
 	var d0 = new Date();
 	var sum = 0;
 	
-	for(var i  =0; i < 1000; i++)
+	for (var i = 0; i < 1000; i++)
 	{
-		if(i % 3 == 0 || i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
 			sum += i;
 		}
